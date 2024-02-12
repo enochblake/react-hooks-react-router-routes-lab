5d8235266ef4eb3059ab22606c1380f53050d5d8
Enoch Kibet <enochblake9@gmail.com>
2024-02-12 16:52:10 +0300

    assignment done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Actors Page</h1>
+    {actors.map((actor, index)=>{
+      return (
+      <div key={index}>
+        {actor.name}
+        {actor.movies.map((movie,index)=>{
+          return (
+          <li key={index}> {movie}</li> )
+        })}
+        </div> )
+    })}
+  </div>
+  )
 }
 
 export default Actors;
